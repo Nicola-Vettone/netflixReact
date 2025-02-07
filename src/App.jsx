0974f898523e9c361bfc452b96bc52e119531671
Nicola Vettone <nicolavettone57@gmail.com>
2025-02-07 11:39:51 +0100

    footer e icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { Container } from "react-bootstrap";
 import "./App.css";
+import ContainerBody from "./components/ContainerBody";
+import Footer from "./components/Footer";
 import TopBar from "./components/TopBar";
 
 function App() {
   return (
-    <>
+    <div className="bg-dark min-vh-100 text-light">
+      <Container fluid className="py-4">
+        <ContainerBody />
+      </Container>
+
       <TopBar />
-    </>
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/components/ContainerBody.jsx
@@ -0,0 +1,6 @@
+import { Container } from "react-bootstrap";
+
+const ContainerBody = () => {
+  return <div className="bg-dark"></div>;
+};
+export default ContainerBody;
