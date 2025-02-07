51ec12b641473be68fc97ef99789e6fa61586448
Nicola Vettone <nicolavettone57@gmail.com>
2025-02-07 16:44:59 +0100

    commentato userpage e settings

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,27 @@ import "./App.css";
 
 import Footer from "./components/Footer";
 import TopBar from "./components/TopBar";
-import Settings from "./components/settings";
-/* import Gallery1 from "./components/Gallery1";
-import Geners from "./components/Geners"; */
+/* import Settings from "./components/settings"; */
+import Gallery1 from "./components/Gallery1";
+import Geners from "./components/Geners";
 
 {
   /*import UserPage from "./components/UserPage";*/
 }
 function App() {
-  const avatar = "https://cdn-icons-png.flaticon.com/512/149/149071.png";
+  /*  const avatar = "https://cdn-icons-png.flaticon.com/512/149/149071.png"; */
 
   return (
     <div className="bg-dark min-vh-100 text-light">
       <Container fluid>
         <TopBar />
         <Container fluid>
-          {/* <Geners />
+          <Geners />
           <Gallery1 film="avengers" title="Avengers" />
           <Gallery1 film="batman" title="Batman" />
-          <Gallery1 film="Harry-potter" title="Harry Potter" /> */}
+          <Gallery1 film="Harry-potter" title="Harry Potter" />
           {/*<UserPage name="Nico" img={avatar} />*/}
-          <Settings name="Nico" img={avatar} />
+          {/*<Settings name="Nico" img={avatar} />*/}
 
           <Footer />
         </Container>
