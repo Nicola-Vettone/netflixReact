c60725afba15965bb2d4ff7a942fcb88c54b2ce8
Nicola Vettone <nicolavettone57@gmail.com>
2025-02-07 16:26:21 +0100

    quasi

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -6,69 +6,74 @@ const Settings = () => {
       <div className="container userSettings">
         <h1>Account</h1>
         <hr />
-        <Row className="d-flex">
-          <Col className="me-4 col-3">
-            <div className="position-relative">
-              <p className="text-secondary">MEMBERSHIP & BILLING</p>
-              <Button variant="outline-secondary" className="border border-none rounded-0 px-4 bg-light text-dark ">
-                Cancel Memebership
-              </Button>
-            </div>
+        <Row className="border-bottom pb-2">
+          <Col className="col-3">
+            <h5 className="text-secondary mt-2">MEMBERSHIP & BILLING</h5>
+            <Button className="mb-2 w-100 bg-body-secondary text-black border-0">Cancel Membership</Button>
           </Col>
-          <Col>
-            <div className="d-flex justify-content-between">
-              <div>
-                <p>
-                  <strong> nicolavettone57@gmail.com</strong>
-                </p>
-                <p className="text-secondary">Password: *********</p>
-                <p className="text-secondary">Phone: 356369325</p>
-              </div>
-              <div className="d-flex flex-column justify-content-end">
-                <p>
-                  <a href="#">Change account email</a>
-                </p>
-                <p>
-                  <a href="#">Change password</a>
-                </p>
-                <p>
-                  <a href="#">Change Phone number</a>
-                </p>
-              </div>
-            </div>
-
-            <hr className="my-4" />
-
-            <div className="d-flex justify-content-between">
-              <div>
+          <Col className="col-9">
+            <Row className="border-bottom my-2">
+              <Col className="col-6">
+                <p className="m-0">nicolavettone57@gmail.com</p>
+                <p className="m-0 text-secondary">Password: ********</p>
+                <p className="text-secondary">Phone: 321 044 1279</p>
+              </Col>
+              <Col className="col-6 text-end">
+                <a href="#" className="text-decoration-none d-block">
+                  Change account email
+                </a>
+                <a href="#" className="text-decoration-none d-block">
+                  Change password
+                </a>
+                <a href="#" className="text-decoration-none d-block">
+                  Change phone number
+                </a>
+              </Col>
+            </Row>
+            <Row className="border-bottom my-2">
+              <Col className="col-6 ps-0">
                 <p>
                   <i className="bi bi-paypal"></i> <strong>Paypal nicolavettone57@gmail.com</strong>
                 </p>
-              </div>
-              <div className="d-flex flex-column">
-                <p>
-                  <a href="#">Update payment info</a>
-                </p>
-                <p>
-                  <a href="#">Billing Details</a>
-                </p>
-              </div>
-            </div>
-
-            <hr className="my-4" />
-            <h3>Autoplay controls</h3>
-            <Form.Check
-              type="checkbox"
-              label="Autoplay next episode in a series on all devices."
-              className="d-flex align-items-center"
-            />
-            <Form.Check
-              type="checkbox"
-              label="Autoplay previews while browsing on all devices."
-              className="d-flex align-items-center mt-3"
-            />
+              </Col>
+              <Col className="col-6 text-end">
+                <a href="#" className="text-decoration-none d-block">
+                  Update payment info
+                </a>
+                <a href="#" className="text-decoration-none d-block">
+                  Billing details
+                </a>
+              </Col>
+            </Row>
+            <Row className="my-2">
+              <Col className="col-12 text-end">
+                <div>
+                  <a href="#" className="text-decoration-none">
+                    Redeem gift card or promo code
+                  </a>
+                </div>
+                <div>
+                  <a href="#" className="text-decoration-none">
+                    Where to buy gift cards
+                  </a>
+                </div>
+              </Col>
+            </Row>
           </Col>
         </Row>
+
+        <hr className="my-4" />
+        <h3>Autoplay controls</h3>
+        <Form.Check
+          type="checkbox"
+          label="Autoplay next episode in a series on all devices."
+          className="d-flex align-items-center"
+        />
+        <Form.Check
+          type="checkbox"
+          label="Autoplay previews while browsing on all devices."
+          className="d-flex align-items-center mt-3"
+        />
         <hr className="my-4" />
         <Row className="d-flex">
           <Button variant="light" className="col-3 rounded-0 px-5 text-dark fs-5 fw-bold">
