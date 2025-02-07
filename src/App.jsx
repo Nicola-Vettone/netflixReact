05f711d46eeaa95735dbd6d1281585b35ee49a04
Nicola Vettone <nicolavettone57@gmail.com>
2025-02-07 13:00:33 +0100

    app

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,6 @@ import "./App.css";
 import Footer from "./components/Footer";
 import TopBar from "./components/TopBar";
 import Gallery1 from "./components/Gallery1";
-import Galleriy2 from "./components/Gallery2";
-import Galleriy3 from "./components/Gallery3";
-
 function App() {
   return (
     <div className="bg-dark min-vh-100 text-light">
